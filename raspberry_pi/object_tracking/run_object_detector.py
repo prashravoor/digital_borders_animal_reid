b5e9fb9e92a59cf2b36b01101505496b6d0c9a7c
Prashanth Ravoor <prash.ravoor125@gmail.com>
2020-03-13 19:01:06 +0530

    Missed adding file

--- raspberry_pi/object_tracking/run_object_detector.py
+++ raspberry_pi/object_tracking/run_object_detector.py
@@ -3,18 +3,25 @@ import sys
 import picamera
 import time
 import picamera.array
+from mqtt_client import TrackPublisher
+
+DEVICE_NAME = 'raspberrypi1'
 
 if __name__ == '__main__':
     args = sys.argv
 
-    if not len(args) == 2:
-        print('Model path needed')
+    if not len(args) == 3:
+        print('Usage: cmd <Model path> <MQTT server IP>')
         exit()
 
     modelpath = args[1]
+    server = args[2]
     detector = ObjectDetector(modelpath, numthreads=4)
     detector.loadModel()
 
+    publisher = TrackPublisher(DEVICE_NAME, server)
+    publisher.register()
+    print('Registered with MQTT server, publishing messages on channel: {}'.format(DEVICE_NAME))
 
     with picamera.PiCamera(resolution=(640,480), framerate=30) as camera:
         with picamera.array.PiRGBArray(camera) as stream:
@@ -27,4 +34,6 @@ if __name__ == '__main__':
                 start = time.time()
                 results = detector.getBoundingBoxes(image)
                 print('Got results: {}, Time: {:.4f}s'.format(results, time.time() - start))
+                for result in results:
+                    publisher.publishDetection(result)
 
