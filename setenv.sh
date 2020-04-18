ac480faed59bc737a913aeafc6eb8dc8a76321fd
Prashanth Ravoor <prash.ravoor125@gmail.com>
2020-04-18 18:45:34 +0530

    THE GREAT REORG

--- setenv.sh
+++ setenv.sh
@@ -3,4 +3,4 @@
 # set base as necessary
 BASE=`pwd`
 
-export PYTHONPATH=$PYTHONPATH:$BASE:"$BASE/raspberry_pi/mqtt":"$BASE/raspberry_pi/object_tracking"
+export PYTHONPATH=$PYTHONPATH:$BASE:"$BASE/utils":"$BASE/mqtt":"$BASE/raspberry_pi":"$BASE/train/truncated_dcnns":"$BASE/train/reid-strong-baseline":"$BASE/central_server/object_tracking"
